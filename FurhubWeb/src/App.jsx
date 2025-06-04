3d40fc1507c887b7a1aeb1c5c942da2369eca756
Jerjet1 <133961975+Jerjet1@users.noreply.github.com>
2025-06-04 14:33:24 +0800

    sample 2

--- FurhubWeb/src/App.jsx
+++ FurhubWeb/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline bg-amber-200">HOYYY!</h1>
+      <h1 className="text-3xl font-bold underline bg-amber-200">HOYYYyy!</h1>
       <h2>Testing</h2>
     </div>
   );
